e8ba67d38d2b6bb8443a249607b19c2876d54848
Jawad Nasar <jawadnasar7886@gmail.com>
2025-07-16 04:22:50 +0100

    Looking for parking Data entered into database

--- js/background.js
+++ js/background.js
@@ -1,86 +1,62 @@
-import * as db from "/js/db_caller.js";
-
 chrome.runtime.onMessage.addListener(function (param, sender, sendResponse) {
     console.log('background param:', param);
-    /* View Visa Details Start */
-    if (param['page_from'] == 'view_visa_details') {
+    /* Looking4parking prices Details Start */
+    if (param['page_from'] == 'looking4_getting_data') {
         switch (param['action']) {
-            case 'add_update_visa_info': // * User Visa Details entry to DB
-                db.send_add_update_visa_information(sendResponse, param['visa_complete_data']);
-                break;
-            case 'check_user_came_from_software': // * Check if called from inside the software or the user came by himself 
-                chrome.storage.local.get(['user_ip'], (res) => {
-                    let hide_show_upd_btn = res.user_ip ? 'show_update_button' : 'hide_update_button';
-                    sendResponse({
-                        'show_hide_button': hide_show_upd_btn
-                    });
-                });
+            case 'get_pricing_data_for_filling': // * Getting rates about all the services from looking4.com
+                fetch_call_db(sendResponse, param['action'], param['pricing_data']);
                 break;
         }
         return true;
     }
-    /* View Visa Details END */
-    /* New Form Filling Start */
-    else if (param['page_from'] == 'enjaz_redirection') {
-        switch (param.action) {
-            case 'get_log_details':
-                db.get_customer_log_details(sendResponse);
-                break;
-            case 'get_user_data_for_filling':
-                db.get_user_new_form_filling_data(sendResponse, param); // Boths params will go in this -> 1.file_reference 2.record_number
-                break;
-            case 'get_user_data_for_ptn':
-                db.get_user_new_form_ptn_data(sendResponse, param); // Boths params will go in this -> 1.file_reference 2.record_number
-                break;
-            case 'get_user_data_for_reg':
-                db.get_user_new_form_reg_data(sendResponse, param); // Boths params will go in this -> 1.file_reference 2.record_number
-                break;
-        }
-        return true;
-    }
-    /* New Form Filling END */
-
-    /* All Enjaz Pages START*/
-    console.log('E_all_pages:', "E_all_pages")
-    if (param['page_from'] == 'E_all_pages') {
-        switch (param.action) {
-            case 'fill_new_enumber':
-                db.fill_new_enumber(sendResponse, param);
-                break;
-            case 'fill_new_ptn':
-                db.fill_new_ptn(sendResponse, param);
-                break;
-            case 'fill_new_reg':
-                db.fill_new_reg(sendResponse, param);
-                break;
-        }
-        return true;
-    }
-    /* All Enjaz Pages END */
+    /* Looking4parking prices Details END */
 
 })
 
+// Function to call the database using fetch API
+function fetch_call_db(call_back_function, action, form_data) {
+    console.clear();
+    try {
+        fetch('http://127.0.0.1:8000/api/update_websites_prices', {
+            method: 'POST',
+            body: JSON.stringify(form_data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+            .then((fetres) => {
+                if (!fetres.ok) {
+                    return fetres.text().then(text => {
+                        throw new Error(`Server responded with ${fetres.status}: ${text}`);
+                    });
+                }
+                return fetres.json(); // parse the JSON response
+            })
+            .then((fetjson) => {
+                if (!fetjson || typeof fetjson !== 'object') {
+                    throw new Error('Invalid JSON response');
+                }
 
-/* Getting URL Before Redirection Start*/
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        let complete_url = new URL(details.url);
-        console.log('complete_url.searchParams.get(action):', complete_url.searchParams.get('action'))
-        if (complete_url.searchParams.get('action')) {
-            chrome.storage.local.set({
-                'f_destination_href': complete_url.href,
-                'f_destination_path': complete_url.pathname,
-                'user_ip': complete_url.searchParams.get('ip')
+                call_back_function({
+                    status: fetjson.status || 'success',
+                    data: fetjson,
+                    form_data: form_data
+                });
+            })
+            .catch(error => {
+                console.log('%c Catch Error: ', 'background: #000; color: #bada55', error);
+                call_back_function({
+                    status: "error",
+                    data: error.message || error
+                });
             });
-            return {
-                requestHeaders: details.requestHeaders
-            }
-        }
-    }, {
-        urls: ["*://services.ksavisa.sa/SmartForm/TraditionalApp?*",
-            "*://services.ksavisa.sa/SmartForm/ElectronicAgreement?*",
-            "*://services.ksavisa.sa/Enjaz/Main?*"
-        ]
+
+    } catch (err) {
+        console.log('%c Catch Error: ', 'background: #9633FF; color: #FFFFFF', action, error)
+        call_back_function({
+            status: "error",
+            data: err
+        });
     }
-);
-/* Getting URL Before Redirection END*/
+}
--- js/looking4_getting_data.js
+++ js/looking4_getting_data.js
@@ -1,14 +1,63 @@
 chrome.storage.local.clear();               // Clearing local storage at the start
 
 $(document).ready(function () {
-    let complete_url = new URL(window.location.href);
-    console.log('Trying to start working in looking4_getting_data.js');
-     chrome.runtime.sendMessage({
+    const pricing_data_obj = new Array();
+
+    /* Observe the page for changes start*/
+    const observer = new MutationObserver((mutations, obs) => {
+        const product_card = document.querySelectorAll('div.product-card__wrapper');
+        if (product_card && product_card.length > 0) {
+            product_card.forEach((card, i) => {
+                const product_title = product_card[i].querySelector('.product-card__mobile-title').innerText.trim()
+                const product_price = product_card[i].querySelector('span[data-testid="productCardPrice"]').innerText.trim();
+                const from_date = formatDateToYMD(document.getElementsByClassName('date-time-picker__value')[0].innerText.trim());
+                const to_date = formatDateToYMD(document.getElementsByClassName('date-time-picker__value')[2].innerText.trim());
+                pricing_data_obj.push({
+                    'product_title': product_title,     // name like "Meet & Greet Parking at London Heathrow Airport, lg parking"
+                    'price': parseFloat(product_price.replace(/[^0-9.]/g, '')),
+                    'from_date': from_date, // from date like "Mon, 20 Nov 2023"
+                    'from_time': to24HourFormat(document.getElementsByClassName('date-time-picker__value')[1].innerText.trim()), // from time like "10:00 AM"
+                    'to_date': to_date, // to date like "Tue, 21 Nov 2023"
+                    'to_time': to24HourFormat(document.getElementsByClassName('date-time-picker__value')[3].innerHTML.trim()), // to time like "10:00 AM"
+                    'airport_name': document.querySelector('.airport-select input').value.trim(), // airport name like "London Heathrow Airport"
+                    'parking_type': '', 
+                    'transfer_time': product_card[i].querySelector('.transfer-text').innerText.trim(),
+                    'website_id':1
+                });
+            });
+
+
+            // Now send the data to background script
+            chrome.runtime.sendMessage({
+                page_from: "looking4_getting_data",
+                action: "get_pricing_data_for_filling",
+                pricing_data: pricing_data_obj
+            }, (res) => {
+                console.log('res:', res);
+            });
+
+            obs.disconnect(); // Stop watching
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+    /* Observe the page for changes end*/
+
+    $('.search-parking-filter__list-desktop-item').html();
+    chrome.runtime.sendMessage({
         page_from: "looking4_getting_data",
-        action: "get_pricing_data_for_filling"
+        action: "get_pricing_data_for_filling",
+        data: {
+            'company_name': 'Looking4Parking',
+            'price': 44
+        }
     }, (res) => {
         console.log('res:', res);
     });
+
 });
 
 window.addEventListener('load', function () {
@@ -17,8 +66,31 @@ window.addEventListener('load', function () {
     // const page_language = complete_url.searchParams.get('lang').toUpperCase();
     const page_language = "ENG";                            // Made it english by default but in future versions user can change it
     const eng_button = this.document.getElementById('en-US');
-    if(eng_button && page_language == "ENG"){
+    if (eng_button && page_language == "ENG") {
         // eng_button.click();
     }
     /* If page is not English page and ENGLISH button is present END*/
 });
+
+
+function formatDateToYMD(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}`; // "2025-08-01"
+}
+
+function to24HourFormat(time12h) {
+    const [time, modifier] = time12h.toLowerCase().split(/(am|pm)/);
+    let [hours, minutes] = time.split(':').map(Number);
+
+    if (modifier === 'pm' && hours !== 12) {
+        hours += 12;
+    } else if (modifier === 'am' && hours === 12) {
+        hours = 0;
+    }
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+}
